f75cde10323b1427acb1f0cdc9fdd830efaaa6b5
Marcos Felipe <mathcosfelipe@gmail.com>
2022-07-07 16:32:38 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 extern crate beginner_tools;
 use beginner_tools::input;
 
+
+fn calculate_hope(variables: Vec<f32>, probabilities: Vec<f32>) {
+    let mut hope: f32 = 0.0;
+    for indexes in 0..variables.len() {
+        hope += variables[indexes] * probabilities[indexes];
+    }
+    println!("{}", hope);
+}
+
 fn main() {
 
     let amount: u32 = loop {
@@ -14,8 +23,7 @@ fn main() {
     let mut probabilities: Vec<f32> = Vec::new();
     
     if amount > 0 {
-    
-        for variable in 1..amount {
+        for variable in 1..amount + 1 {
             let variable_value: f32 = loop {
                 let input_variable_message: String = format!("Input the value of variable x{}: ", variable);
                 if let Ok(variable_value_validity) = input(&input_variable_message) {
@@ -36,13 +44,23 @@ fn main() {
             };
             probabilities.push(probability_value);
         }
+
+        let mut total_probabilities: f32 = 0.0;
+
+        for probability_index in 0..probabilities.len() {
+            total_probabilities += probabilities[probability_index];
+        }
+
+        if total_probabilities != 1.0 {
+            println!("Error! The sum of probabilities mut be 1.0. Try again.");
+            main();
+        } else {
+            calculate_hope(variables, probabilities);
+        }
     
     } else {
         println!("You must enter at least one variable. Try again.");
         main();
     }
-
-    println!("{:?}", variables);
-    println!("{:?}", probabilities);
 
 }
