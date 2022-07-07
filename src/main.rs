e4030eeb43506304372a8af17baab8e21e76072b
Marcos Felipe <mathcosfelipe@gmail.com>
2022-07-07 12:30:05 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,24 @@ fn main() {
         };
         println!("Invalid value! Input a integer number. Try again.");
     };
+
+    let mut variables: Vec<f32> = Vec::new();
+    let mut _probabilities: Vec<f32> = Vec::new();
     
     if amount > 0 {
     
         for variable in 1..amount {
-            let variable_value: u32 = loop {
-                if let Ok(variable_value_validity) = input("Input the value of variable : ") {
+            let variable_value: f32 = loop {
+                let input_variable_message = format!("Input the value of variable x{}: ", variable);
+                if let Ok(variable_value_validity) = input(&input_variable_message) {
                     break variable_value_validity
                 };
                 println!("Invalid value! Input a integer number. Try again.");
             };
+            variables.push(variable_value);
         }
+
+
     
     } else {
         println!("You must enter at least one variable. Try again.");
