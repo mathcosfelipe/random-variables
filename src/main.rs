b617667fd1527dfad109b0a0894767b331a63c95
Marcos Felipe <mathcosfelipe@gmail.com>
2022-07-08 23:21:23 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 extern crate beginner_tools;
 use beginner_tools::input;
 
-mod calculate_resum;
+mod resum;
 
 fn main() {
 
+    let variable_name: String = loop {
+        if let Ok(variable_name_validity) = input("Input the name of variable: ") {
+            break variable_name_validity
+        };
+        println!("Error! Invalid value. Try again.");
+        main();
+    };
+
     let amount: u32 = loop {
-        if let Ok(amount_validity) = input("Input the amount of variables (x): ") {
+        let variable_name_input: String = format!("Input the amount of variables {}: ", variable_name);
+        if let Ok(amount_validity) = input(&variable_name_input) {
             break amount_validity
         };
         println!("Invalid value! Input a integer number. Try again.");
     };
 
-    let mut variables: Vec<f32> = Vec::new();
+    let mut variables: Vec<i32> = Vec::new();
     let mut probabilities: Vec<f32> = Vec::new();
     
     if amount > 0 {
         for variable in 1..amount + 1 {
-            let variable_value: f32 = loop {
-                let input_variable_message: String = format!("Input the value of variable x{}: ", variable);
+            let variable_value: i32 = loop {
+                let input_variable_message: String = format!("Input the value of variable {}{}: ", variable_name, variable);
                 if let Ok(variable_value_validity) = input(&input_variable_message) {
                     break variable_value_validity
                 };
@@ -29,7 +38,7 @@ fn main() {
 
         for probability in 0..variables.len() {
             let probability_value: f32 = loop {
-                let input_probability_message: String = format!("Input the probability of variable x{}: ", variables[probability]);
+                let input_probability_message: String = format!("Input the probability of variable {}{}: ", variable_name, variables[probability]);
                 if let Ok(probability_value_validity) = input(&input_probability_message) {
                     break probability_value_validity
                 };
@@ -48,7 +57,7 @@ fn main() {
             println!("Error! The sum of probabilities mut be 1.0. Try again.");
             main();
         } else {
-            calculate_resum::calculate_media(variables, probabilities);
+            resum::calculate_media(variables, probabilities);
         }
     
     } else {
